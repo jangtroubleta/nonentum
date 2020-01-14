020320051eeff057a9c96bbd88c9820ecedae56a
jangtroubleta <jhh9507@gmail.com>
2020-01-14 09:23:01 +0900

    creating focus#01

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const clockContainer = document.querySelector(".clock");
 const clockTitle = clockContainer.querySelector("h1");
 const greetingContainer = document.querySelector(".greeting");
+const focusContainer = document.querySelector(".focus");
 
 function getTime() {
     const now = new Date();
@@ -15,7 +16,7 @@ function setGreeting(user = null) {
     const form = document.createElement("form");
     const input = document.createElement("input");
     input.setAttribute("type", "text");
-    input.setAttribute("placeholder","What is your name");
+    input.setAttribute("placeholder","What is your name?");
     input.value = user;
     localStorage.removeItem("currentUser");
     form.appendChild(input);
@@ -32,11 +33,12 @@ function setGreeting(user = null) {
 function getGreeting() {
     const now = new Date();
     const hour = now.getHours();
-    const h3 = document.createElement("h3");
+    const h2 = document.createElement("h2");
     const button = document.createElement("button");
+    
     let greeting = "";
 
-    if(hour >= 5 && hour < 12) {
+    if (hour >= 5 && hour < 12) {
         greeting = "Good morning!,";
     } else if (hour >= 12 && hour < 17) {
         greeting = "Good afternoon!,";
@@ -44,29 +46,73 @@ function getGreeting() {
         greeting = "Good evening!,";
     }
 
-    h3.innerHTML = `${greeting} ${localStorage.getItem("currentUser")}.`;
-    greetingContainer.appendChild(h3);
+    h2.innerHTML = `${greeting} ${localStorage.getItem("currentUser")}.`;
+    greetingContainer.appendChild(h2);
     button.innerHTML = "edit";
-    h3.appendChild(button);
+    h2.appendChild(button);
 
     button.addEventListener("click", () => {
-        greetingContainer.removeChild(h3);
+        greetingContainer.removeChild(h2);
         setGreeting(localStorage.getItem("currentUser"));
+    });
+}
+
+function setFocus() {
+    const form = document.createElement("form");
+    const h3 = document.createElement("h3")
+    const input = document.createElement("input");
+   
+    focusContainer.appendChild(form);
+    
+    input.setAttribute("type", "text");
+    input.setAttribute("placeholder","Your focus!");
+    h3.innerHTML = "What is your main focus for today?";
+    
+    form.appendChild(h3);
+    form.appendChild(input);
+    
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        localStorage.setItem("currentFocus", input.value.trim());
+        focusContainer.removeChild(form);
+        getFocus();
+    });
+}
+
+function getFocus() {
+    const h3 = document.createElement("h3");
+    const button = document.createElement("button");
+    const input = document.createElement("input");
+
+    input.setAttribute("type", "checkbox");
+    h3.innerHTML = localStorage.getItem("currentFocus");
+    button.innerHTML = "X";
+
+    focusContainer.appendChild(input);
+    focusContainer.appendChild(h3);
+    focusContainer.appendChild(button);
+
+    input.addEventListener("click", () => {
+        (input.checked) ?  h3.classList.toggle("line-through") : h3.classList.toggle("line-through");
+    });
+
+    button.addEventListener("click", () => {
+        focusContainer.removeChild(input);
+        focusContainer.removeChild(button);
+        focusContainer.removeChild(h3);
+        localStorage.removeItem("currentFocus");
+        setFocus();
     });
 }
 
 function init() {
     getTime();
-
-    if(localStorage.getItem("currentUser") !== null) {
-        getGreeting()
-    } else {
-        setGreeting() 
-    }
+    ( localStorage.getItem("currentUser") ) !== null ? getGreeting() : setGreeting();
+    ( localStorage.getItem("currentFocus")) !== null ? getFocus() : setFocus();
 }
 
 init();
 setInterval(getTime, 1000);
-if(localStorage.getItem("currentUser") !== null) {
+if (localStorage.getItem("currentUser") !== null) {
     setInterval(getGreeting, 1000*60*60);
 }
